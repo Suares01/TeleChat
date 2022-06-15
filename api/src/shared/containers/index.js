d4bb7e1a9733a5e537fb8b49960cf3e07935447c
Suares01 <hub_suares.01@hotmail.com>
2022-06-14 23:16:40 -0300

    fix(api): adjust imports and add UsersRepository

--- api/src/shared/containers/index.js
+++ api/src/shared/containers/index.js
@@ -1,9 +1,11 @@
 import { Container } from 'typedi';
 
-import { ExpressServer } from '../../components/http/express/Server';
-import { BcryptjsHashService } from '../../services/hashService';
-import { JsonwebtokenTokenService } from '../../services/TokenService';
+import { PrismaUsersRepository } from '../../components/users/repositories';
+import { BcryptjsHashService } from '../../components/users/services/hashService';
+import { JsonwebtokenTokenService } from '../../components/users/services/TokenService';
+import { ExpressServer } from '../../http/express/Server';
 
 Container.set('Server', new ExpressServer());
 Container.set('HashService', new BcryptjsHashService());
 Container.set('TokenService', new JsonwebtokenTokenService());
+Container.set('UsersRepository', new PrismaUsersRepository());
