65eb078a01855417193b6a0822e42537a48441b4
Suares01 <hub_suares.01@hotmail.com>
2022-07-01 19:02:41 -0300

    fix(api): add findFirst, findMany, delete and update methods

--- api/src/components/users/repositories/prisma/PrismaContactsRepository.js
+++ api/src/components/users/repositories/prisma/PrismaContactsRepository.js
@@ -18,4 +18,51 @@ export class PrismaContactsRepository {
 
     return contact;
   }
+
+  async findFirst(phoneNumber, userId) {
+    const contact = await this.#model.findFirst({
+      where: {
+        phoneNumber,
+        userId,
+      },
+    });
+
+    return contact;
+  }
+
+  async findMany(userId) {
+    const contacts = await this.#model.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        name: 'asc',
+      },
+    });
+
+    return contacts;
+  }
+
+  async delete(id) {
+    const deletedContact = await this.#model.delete({
+      where: {
+        id,
+      },
+    });
+
+    return deletedContact;
+  }
+
+  async update(id, newName) {
+    const updatedContact = await this.#model.update({
+      where: {
+        id,
+      },
+      data: {
+        name: newName,
+      },
+    });
+
+    return updatedContact;
+  }
 }
